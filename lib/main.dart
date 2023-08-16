ef1a6977f6445703e58a04376daf647cd0454b49
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-08-16 11:19:46 +0000

    project components

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+main() => {
+      runApp(const FundFlow()),
+    };
+
+class FundFlow extends StatelessWidget {
+  const FundFlow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
